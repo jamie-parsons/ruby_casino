124843c6ef9a89332852b5e5fd88263737dd17f8
Jacob Wheeler <wheeler.jacob.r@gmail.com>
2017-05-22 14:21:02 -0600

    wallet parameters

--- wallet.rb
+++ wallet.rb
@@ -2,8 +2,23 @@ class Wallet
   attr_accessor :amount
 
   def initialize(amount)
+    if
+      @amount == Int
+    else
+      puts "Please enter a number"
+    end
+
+    if
+      @amount > 1000
+      puts "You have exceeded the limit"
+    elsif
+      @amount < 1
+      puts "You do not have enough money to play"
+    else
     @amount = amount
+    end
   end
+
 
   def update_money(bet, win = true)
     if win
@@ -14,4 +29,6 @@ class Wallet
     puts "Your wallet now has #{amount}"
   end
 
+#validation for range
+#validation enough money in their wallet
 end
