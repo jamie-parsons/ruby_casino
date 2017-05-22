c0e1790ef3cda766038c81e0ddb08cb3b667226d
Jacob Wheeler <wheeler.jacob.r@gmail.com>
2017-05-22 10:30:28 -0600

    wallet class

--- casino.rb
+++ casino.rb
@@ -6,10 +6,12 @@ require 'colorize'
 require_relative 'player'
 
 class Casino
+attr_accessor :player
 
   def initialize
   puts 'Welcome to our Ruby Casino!'
   @player = Player.new
+  binding.pry
   end
 end
 
