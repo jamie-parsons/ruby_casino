378430bfdc6de50bdf4e5669c41c55174a7820c3
Zoe Larsen <zlarsen.zl@gmail.com>
2017-05-22 10:53:42 -0600

    heads tails added

--- casino.rb
+++ casino.rb
@@ -4,6 +4,7 @@ require 'colorize'
 
 #Pull in other classes you may need
 require_relative 'player'
+require_relative 'head_tails'
 
 class Casino
 attr_accessor :player
@@ -11,7 +12,15 @@ attr_accessor :player
   def initialize
   puts 'Welcome to our Ruby Casino!'
   @player = Player.new
-  binding.pry
+  casino_menu
+  end
+
+  def casino_menu
+    #Casino option
+    #bank roll
+    #player status
+    #quit
+    HeadsTails.new(player)
   end
 end
 
---
+++ head_tails.rb
@@ -0,0 +1,10 @@
+class HeadsTails
+  def initialize(player)
+    puts "Welecome to Heads Tails"
+    puts "#{player.name} you have a balance of: #{player.wallet.amount}"
+  end
+
+  #play method
+  #flip coin method
+  
+end
