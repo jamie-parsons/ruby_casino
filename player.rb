933ce177780d4f528486a917cf6d4d7ee3be2d1a
Zoe Larsen <zlarsen.zl@gmail.com>
2017-05-22 14:02:35 -0600

    No more gender

--- player.rb
+++ player.rb
@@ -1,7 +1,7 @@
 require_relative 'wallet'
 
 class Player
-  attr_accessor :name, :age, :gender, :wallet
+  attr_accessor :name, :age, :wallet
 
   def initialize
     #need to think about how to implement the wallet
@@ -10,8 +10,6 @@ class Player
     @name = gets.strip
     puts "What is your age player?"
     @age = gets.strip.to_i
-    puts "What is your gender?"
-    @gender = gets.strip
     puts 'How much money are you playing with?'
     @wallet = Wallet.new(gets.to_f)
   end
