b62716dd1559deb0285e41368a712010fab59a54
Zoe Larsen <zlarsen.zl@gmail.com>
2017-05-23 09:39:24 -0600

    High low game adjustments

--- high_low.rb
+++ high_low.rb
@@ -1,9 +1,11 @@
+require 'pry'
+
 require_relative 'player'
 require_relative 'deck'
 require_relative 'card'
 
 class HighLow
-  attr_accessor :player, :bet, :deck
+  attr_accessor :player, :bet, :deck, :card_1best, :card_2best
   def initialize(player)
     # @player = Player.new
     @deck = Deck.new.shuffle_cards
@@ -23,19 +25,18 @@ class HighLow
     choice = gets.to_i
     card_2 = @deck.pop
     puts "The next card is #{card_2.rank}"
-    card_rank(card_1, card_2)
-  end
-
-  def case
+    card_check1(card_1)
+    card_check2(card_2)
+    binding.pry
     case choice
     when 1
-      if card_1.rank.to_i < card_2.rank.to_i
+      if @card_1best < @card_2best
         puts "You win"
       else
         puts "You Lose"
       end
     when 2
-      if card_1.rank.to_i > card_2.rank.to_i
+      if @card_1best > @card_2best
         puts "You lose"
       else
         puts "You win"
@@ -45,20 +46,28 @@ class HighLow
     end
   end
 
-  def card_rank(card_1, card_2)
-    if card_1.rank == 'J' || card_2.rank == 'J'
-      card_1.rank = 11
-      card_2.rank = 11
-    elsif card_1.rank == 'Q' || card_2.rank == 'Q'
-      card_1.rank = 12
-      card_2.rank = 12
-    elsif card_1.rank == 'K' || card_2.rank == 'K'
-      card_1.rank = 13
-      card_2.rank = 13
+  def card_check1(card_1)
+    if card_1.rank == 'J'
+      @card_1best = 11
+    elsif card_1.rank == 'Q'
+      @card_1best = 12
+    elsif card_1.rank == 'K'
+      @card_1best = 13
     else
-      card_1.rank.to_i
-      card_2.rank.to_i
+      @card_1best = card_1.rank.to_i
+    end
   end
-    case()
-end
+
+    def card_check2(card_2)
+      if card_2.rank == 'J'
+        @card_2best = 11
+      elsif card_2.rank == 'Q'
+        @card_2best = 12
+      elsif card_2.rank == 'K'
+        @card_2best = 13
+      else
+        @card_2best = card_2.rank.to_i
+      end
+    end
+
 end
