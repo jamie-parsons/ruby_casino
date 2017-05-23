5bb2e4d23fdaf9700fadeefa8359de66b17d9e76
Jacob Wheeler <wheeler.jacob.r@gmail.com>
2017-05-23 15:52:30 -0600

    blackjack working

--- blackjack.rb
+++ blackjack.rb
@@ -18,12 +18,12 @@ class Blackjack
   # Get all the cards in the deck
     player_card_1 = card_check(d.show_card.rank)
     player_card_2 = card_check(d.show_card.rank)
-    @player_total_1 = player_card_1 + player_card_2
-    puts "Your hand total #{@player_total_1}"
-    dealer_card_1 = card_check(d.show_card.rank)
-    dealer_card_2 = card_check(d.show_card.rank)
-    @dealer_total_1 = dealer_card_1 + dealer_card_2
-    puts "Your dealer's hand is #{@dealer_total_1}"
+    @player_total_new = player_card_1 + player_card_2
+    puts "Your hand total #{@player_total_new}"
+    dealer_card_1 = card_check(d.show_card.rank, true)
+    dealer_card_2 = card_check(d.show_card.rank, true)
+    @dealer_total_new = dealer_card_1 + dealer_card_2
+    puts "The dealer's hand total is #{@dealer_total_new}"
     puts "How much would you like to bet?"
     @bet = gets.to_i
     draw(player)
@@ -32,81 +32,74 @@ class Blackjack
   def draw(player)
     d = Deck.new
     puts "Would you like to hit? Y for yes, N for No."
-    card_question = gets.strip
+    card_question = gets.strip.upcase
     if card_question == "Y"
       player_card_3 = card_check(d.show_card.rank)
-      @player_total_new = @player_total_1 + player_card_3
+      @player_total_new += player_card_3
       puts "Your hand total #{@player_total_new}"
-      # if @dealer_total_1 < 18
-      #   dealer_card_3 = card_check(d.show_card.rank)
-      #   @dealer_total_new = @dealer_total_1 + dealer_card_3
-      #   puts "The dealer's hand is #{@dealer_total_new}"
-      # else
-      #   puts "Dealer stays at #{@dealer_total_1}"
-      # end
+      dealer_draw_stay(player)
       bust_draw(player)
     elsif card_question == "N" #compare hands
-      "You stay with #{@player_total_new}"
-      if @dealer_total_1 < 18
-          dealer_card_3 = card_check(d.show_card.rank)
-          @dealer_total_new = @dealer_total_1 + dealer_card_3
-          puts "The dealer's hand is #{@dealer_total_new}"
-          if @player_total_new < @dealer_total_1
-            "Dealer wins with #{@dealer_total_1}!"
-            player.wallet.update_money(@bet, false)
-          else
-            "You win with #{player_total_new}"
-            player.wallet.update_money(@bet, true)
-          end
-      else
-          puts "Dealer stays at #{@dealer_total_1}"
-
+      @player_total_new 
+      puts "You stay with #{@player_total_new}"
+      dealer_draw_stay(player)
+      bust_draw(player)
     else
       puts "Error, please make a valid entry!"
       draw(player)
     end
   end
 
+  def dealer_draw_stay(player)
+    d = Deck.new
+    if @dealer_total_new < 18
+        dealer_card_3 = card_check(d.show_card.rank, true)
+        @dealer_total_new += dealer_card_3
+        puts "The dealer's hand is #{@dealer_total_new}"
+    else @dealer_total_new > 18
+        puts "Dealer stays at #{@dealer_total_new}"
+    end
+  end
+
   def bust_draw(player)
     if @player_total_new > 21
-      puts "You bust!"
+      puts "You bust! Dealer Wins"
       player.wallet.update_money(@bet, false)
     elsif @dealer_total_new > 21
-      puts "Dealer busts, you win!"
+      puts "Dealer busts! You Win!"
+      player.wallet.update_money(@bet, true)
+    elsif @dealer_total_new = 21
+      puts "Dealer Wins!"
+      player.wallet.update_money(@bet, false)
+    elsif @player_total_new > @dealer_total_new
+      puts "You Win!"
+      player.wallet.update_money(@bet, true)
+    elsif @player_total_new == 21
+      puts "You Win!"
       player.wallet.update_money(@bet, true)
       BlackJack.new(player)
-    elsif @player_total_new == 21 && @dealer_total_new == 21
-      puts "Bust!"
-      player.wallet.update_money(@bet, false)
-    # elsif
-
-
-    # elsif player_total_1 < 21
-      # puts "Would you like to draw another card?"
-      # puts "Type y for yes and n for no"
-      # card_question = gets.strip
-      # if card_question == "y"
-      #   player_card_1
       end
     end
 
-
-  def card_check(card)
+  def card_check(card, isDealer = false)
     if card == "J" || card == "Q" || card == "K"
       10
     elsif card == 'A'
-      puts "Choose 1 or 11"
-      ace_input = gets.strip.to_i
-      binding.pry
-       if ace_input == 1   #not adding totals to wallet? added else condition => didn't fix
-         1
-       elsif ace_input == 11
-          11
-       else
-        puts "Error, please try again!"
-        card_check
-       end
-       card.to_i
+      if !isDealer
+        puts "Choose 1 or 11"
+        ace_input = gets.strip.to_i
+        if ace_input == 1
+          card = 1
+        elsif ace_input == 11
+          card = 11
+        else
+          puts "Error, please try again!"
+          card_check
+        end
+      else
+        card = 11
+      end
+      card.to_i
     else
       card.to_i
     end
